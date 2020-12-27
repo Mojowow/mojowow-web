27aeb8674092d438ad72c3b853c4576251aa3f52
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2020-12-27 14:14:55 +0100

    fixed type to actually be able to compile

--- pages/index.vue
+++ pages/index.vue
@@ -73,7 +73,7 @@ export default Vue.extend({
 
 <script lang="ts">
 export default {
-  async asyncData({ $content }) {
+  async asyncData({ $content }: { $content: Function }) {
     const page = await $content('home').fetch()
     return { page }
   },
